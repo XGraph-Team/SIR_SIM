4de8e43c504ad9d4097d3b19ea66773e7eb311f7
Zijian Zhang <zz242@msstate.edu>
2024-03-11 21:20:01 -0500

    Update api-for-open-llm_mock_openai_request.py

--- api-for-open-llm_mock_openai_request.py
+++ api-for-open-llm_mock_openai_request.py
@@ -7,18 +7,17 @@ model = "long-llama"
 
 def create_chat_completion(model, messages, stream=False):
     data = {
-        "model": model,  # 模型名称
-        "messages": messages,  # 会话历史
-        "stream": stream,  # 是否流式响应
-        "max_tokens": 100,  # 最多生成字数
-        "temperature": 0.8,  # 温度
-        "top_p": 0.8,  # 采样概率
+        "model": model,  
+        "messages": messages,  
+        "stream": stream,  
+        "max_tokens": 100,  
+        "temperature": 0.8,  
+        "top_p": 0.8, 
     }
 
     response = requests.post(f"{base_url}/v1/chat/completions", json=data, stream=stream)
     if response.status_code == 200:
         if stream:
-            # 处理流式响应
             for line in response.iter_lines():
                 if line:
                     decoded_line = line.decode('utf-8')[6:]
@@ -29,7 +28,6 @@ def create_chat_completion(model, messages, stream=False):
                     except:
                         print("Special Token:", decoded_line)
         else:
-            # 处理非流式响应
             decoded_line = response.json()
             print(decoded_line)
             content = decoded_line.get("choices", [{}])[0].get("message", "").get("content", "")
